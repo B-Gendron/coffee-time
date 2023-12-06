2b872ae7556c63afe5dd5fa7cb8c71af430bad41
Barbara Gendron <95307996+B-Gendron@users.noreply.github.com>
2023-12-06 22:11:51 +0100

    add small message

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ function respondToInvitation(response) {
     var timeInput = document.getElementById('time-input');
 
     if (response === 'yes') {
-        responseMessage.textContent = 'Great! Looking forward to it. ☕';
+        responseMessage.setAttribute('style', 'white-space: pre;');
+        responseMessage.textContent = "Bon, il est 22h et j'ai la flemme d'écrire en anglais. J'aurais aimé prendre le temps de créer une boîte de dialogue pour dire ce qui va suivre,\r\n";
+        responseMessage.textContent += "mais il est toujours 22h et j'ai toujours la flemme donc : quand tu cliques sur 'Validate', je reçois un mail avec l'horaire.\r\n";
+        responseMessage.textContent += "Boom, c'est trop fort l'info (en vrai merci ChatGPT, mais c'est trop fort quand même).\r\n";
+        responseMessage.textContent += "À +,\r\n";
+        responseMessage.textContent += "Vivent les GAN.";
         timeInput.style.display = 'block';
     } else if (response === 'no') {
         responseMessage.textContent = 'Maybe next time. 😔';
@@ -20,11 +25,11 @@ function validateAndSendEmail() {
     
     if (timeInput.trim() !== "") {
         // Simulate sending an email (for educational purposes only)
-        sendEmail(timeInput);
+        sendAnEmail(timeInput);
     }
 }
 
-function sendEmail(coffeeTime) {
+function sendAnEmail(coffeeTime) {
     emailjs.init("y9J6etPPp_RmmWuxU");
     var templateParams = {
         to_email: "barbara.gendron@hotmail.fr", // Replace with your email address
